4571c4fb29fba6d952fd828b1970cd8683bd2c05
aman <amansaini1407@gmail.com>
2025-09-25 21:39:32 +0530

    Product Approved Added

--- src/app/_services/productOverviewService/product-overview.service.ts
+++ src/app/_services/productOverviewService/product-overview.service.ts
@@ -3,22 +3,40 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { API_AUTHORIZA_URL } from '../../constants/Constants';
 
-
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ProductOverviewService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  getUnderReviewProductService(request: any, username: any): Observable<any> {
+    return this.http.post(
+      API_AUTHORIZA_URL +
+        'productOverviewController/' +
+        'getUnderReviewProduct?page=' +
+        request.page +
+        '&size=' +
+        request.size +
+        '&username=' +
+        username,
+      httpOptions
+    );
+  }
 
-
-    getUnderReviewProduct(request:any , username:any): Observable<any> {
-    return this.http.post(API_AUTHORIZA_URL + "productOverviewController/"+ 
-                          'getUnderReviewProduct?page='+request.page + '&size=' +request.size+ '&username=' +username ,
-                           httpOptions);
+  getApprovedProductService(request: any, username: any): Observable<any> {
+    return this.http.post(
+      API_AUTHORIZA_URL +
+        'productOverviewController/' +
+        'getApprovedProduct?page=' +
+        request.page +
+        '&size=' +
+        request.size +
+        '&username=' +
+        username,
+      httpOptions
+    );
   }
 }
--- src/app/seller-panel/product-overview/product-overview.component.ts
+++ src/app/seller-panel/product-overview/product-overview.component.ts
@@ -11,18 +11,6 @@ import { TokenStorageService } from '../../_services/token-storage.service';
   styleUrl: './product-overview.component.css',
 })
 export class ProductOverviewComponent {
-  isChecked = true;
-  underReviewData: any;
-
-
-  totalElements: number = 0;
-  currentPage: number = 1;
-  itemsPerPage: number = 10;
-
-  // is Skeleton Loader Valid
-  isLoaderValid:any =false;;
-
-
 
   constructor(
     private productOverviewService: ProductOverviewService,
@@ -33,35 +21,51 @@ export class ProductOverviewComponent {
 
   ngOnInit(): void {}
 
+
+
+
+
+
+
   // UNDER REVIEW PRODUCT STARTING
+
+  underReviewData: any;
+  underReview_totalElements: number = 0;
+  underReview_currentPage: number = 1;
+  underReview_itemsPerPage: number = 10;
+  // is Skeleton Loader Valid
+  underReview_skeleton:any =false;
+
   getProductUnderReview() {
     this.getUnderReviewProduct({ page: "0", size: "10" });
   }
 
   getUnderReviewProduct(request: any) {
-    this.isLoaderValid = true;
+    this.underReview_skeleton = true;
 
     const user = this.tokenStorageService.getUser();
 
     this.productOverviewService
-      .getUnderReviewProduct(request, user.username)
+      .getUnderReviewProductService(request, user.username)
       .subscribe({
         next: (res: any) => {
+          console.log(res);
+          
           this.underReviewData = res.data.content;
-          this.totalElements = res.data.totalElements;
-          this.currentPage = res.data.pageable.pageNumber;
-          this.itemsPerPage = res.data.pageable.pageSize;
-          this.isLoaderValid = false;
+          this.underReview_totalElements = res.data.totalElements;
+          this.underReview_currentPage = res.data.pageable.pageNumber;
+          this.underReview_itemsPerPage = res.data.pageable.pageSize;
+          this.underReview_skeleton = false;
 
-          this.toast.success({
-            detail: 'Success',
-            summary: 'Data Fetch Success',
-            position: 'bottomRight',
-            duration: 3000,
-          });
+          // this.toast.success({
+          //   detail: 'Success',
+          //   summary: 'Data Fetch Success',
+          //   position: 'bottomRight',
+          //   duration: 3000,
+          // });
         },
         error: (err: any) => {
-          this.isLoaderValid = false;
+          this.underReview_skeleton = false;
           this.toast.error({
             detail: 'Error',
             summary: err.error.data?.message || 'Something went wrong',
@@ -73,17 +77,95 @@ export class ProductOverviewComponent {
 }
 
 
-nextPage(event: PageEvent) {
-    this.currentPage = event.pageIndex;
-    this.itemsPerPage = event.pageSize;
+nextPageUnderReview(event: PageEvent) {
+    this.underReview_currentPage = event.pageIndex;
+    this.underReview_itemsPerPage = event.pageSize;
 
     const request = {
-        page: this.currentPage.toString(),
-        size: this.itemsPerPage.toString()
+        page: this.underReview_currentPage.toString(),
+        size: this.underReview_itemsPerPage.toString()
     };
-
     this.getUnderReviewProduct(request);
 }
-
    // UNDER REVIEW PRODUCT ENDING
+
+// =============================================================================
+// =============================================================================
+
+
+// PRODUCT APPROVED STARTING
+  approvedData: any;
+  approved_totalElements: number = 0;
+  approved_currentPage: number = 1;
+  approved_itemsPerPage: number = 10;
+  // is Skeleton Loader Valid
+  approved_skeleton:any =false;
+
+  getProductApproved() {
+    this.getApprovedProduct({ page: "0", size: "10" });
+  }
+
+  getApprovedProduct(request: any) {
+    this.approved_skeleton = true;
+
+    const user = this.tokenStorageService.getUser();
+
+    this.productOverviewService
+      .getApprovedProductService(request, user.username)
+      .subscribe({
+        next: (res: any) => {
+          console.log(res);
+          
+          this.approvedData = res.data.content;
+          this.approved_totalElements = res.data.totalElements;
+          this.approved_currentPage = res.data.pageable.pageNumber;
+          this.approved_itemsPerPage = res.data.pageable.pageSize;
+          this.approved_skeleton = false;
+
+          // this.toast.success({
+          //   detail: 'Success',
+          //   summary: 'Data Fetch Success',
+          //   position: 'bottomRight',
+          //   duration: 3000,
+          // });
+        },
+        error: (err: any) => {
+          this.approved_skeleton = false;
+          this.toast.error({
+            detail: 'Error',
+            summary: err.error.data?.message || 'Something went wrong',
+            position: 'bottomRight',
+            duration: 3000,
+          });
+        },
+      });
+}
+
+
+nextPageApproved(event: PageEvent) {
+    this.approved_currentPage = event.pageIndex;
+    this.approved_itemsPerPage = event.pageSize;
+
+    const request = {
+        page: this.approved_currentPage.toString(),
+        size: this.approved_itemsPerPage.toString()
+    };
+    this.getApprovedProduct(request);
+}
+//PRODUCT APPROVED ENDING...
+
+
+
+
+
+// =============================================================================
+// =============================================================================
+
+
+
+
+
+
+
+
 }
